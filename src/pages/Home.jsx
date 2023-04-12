0d7f46db5c9716c98d903725442d9deee24b88a0
ashikahmedfahim <41462787+ashikahmedfahim@users.noreply.github.com>
2023-04-12 16:42:27 +0600

    image slider added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { getSearchResult, setSelectedResult } from "../slices/searchSlice";
 import Container from "../components/Container";
 import { useNavigate } from "react-router-dom";
+import imageOne from "../images/one.jpg";
+import imageTwo from "../images/two.jpg";
+import imageThree from "../images/three.jpg";
+
+const images = [imageOne, imageTwo, imageThree];
 
 const Home = () => {
+  const [currentImg, setCurrentImg] = useState(0);
   const [searchedTerm, setSearchedTerm] = useState("");
   const { searchResult } = useSelector((state) => state.search);
   const dispatch = useDispatch();
@@ -21,9 +27,25 @@ const Home = () => {
     }
   }, [searchedTerm]);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCurrentImg((prev) => (prev + 1) % 3);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [currentImg]);
+
   return (
     <Container>
-      <div className="flex justify-center items-center m-5">
+      <div className="flex flex-col justify-center items-center m-5">
+        <div className="mb-10">
+          <h1 className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600">
+            Music App
+          </h1>
+          <p className="text-transparent text-xl bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600 mt-5">
+            An environment where you can find your favourite music
+          </p>
+        </div>
         <div className="min-w-full md:min-w-[500px] relative">
           <input
             type="text"
@@ -54,6 +76,13 @@ const Home = () => {
               )}
             </div>
           )}
+        </div>
+        <div className="min-w-full md:min-w-[500px]">
+              <img
+                src={images[currentImg]}
+                alt="music"
+                className="w-full h-96 object-cover rounded-lg mt-10"
+              />
         </div>
       </div>
     </Container>
